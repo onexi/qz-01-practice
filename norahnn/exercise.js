df76a1e098b1c1432fb59b8642ddfba02c39d208
norahnn <norah15@mit.edu>
2017-03-12 21:27:52 -0400

    more changes

--- norahnn/exercise.js
+++ norahnn/exercise.js
@@ -83,7 +83,7 @@ exercise.six = function(arr){
     // ----------------------------------------
     var output=[];
     for (var i=0;i<arr.length;i++){
-        if (arr[i]%2===1){
+        if (arr[i]%2===0){
             output.push(arr[i]);
         }
     }
